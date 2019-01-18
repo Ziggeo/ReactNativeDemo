7693f80744415ad99c14d53a76ec1374dce0c406
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-01-18 15:22:28 +0300

    Added gesture-handler dependency

--- android/app/src/main/java/com/reactnativedemo/MainApplication.java
+++ android/app/src/main/java/com/reactnativedemo/MainApplication.java
@@ -7,7 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.ziggeo.ZiggeoPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.ziggeo.modules.ZiggeoPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new ZiggeoPackage()
+                    new ZiggeoPackage(),
+                    new RNGestureHandlerPackage()
             );
         }
     };
