0dd722dd2c47b0c33ca3c0f1b643502c8bbc0009
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-02-21 14:26:15 +0300

    Removed hardcoded test values

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NativeEventEmitter, NativeModules } from 'react-native';
 import Ziggeo from 'react-native-ziggeo-library';
-//TODO ZIGGEO_APP_TOKEN
+
 export default class App extends React.Component {
     async record() {
-        var appToken = "d541dc6b1351d6424b04fb8415658e0d"; 
+        var appToken = "ZIGGEO_APP_TOKEN";
         Ziggeo.setAppToken(appToken);
         Ziggeo.setCameraSwitchEnabled(true);
         Ziggeo.setCoverSelectorEnabled(false);
