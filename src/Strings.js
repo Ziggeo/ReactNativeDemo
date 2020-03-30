3ace2c89d326d16300914f8ff9c339d44ff75e68
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-03-30 16:13:21 +0300

    About screen partially implemented

--- src/Strings.js
+++ src/Strings.js
@@ -63,31 +63,36 @@ export default {
   hintTitle: 'Title for the recording (optional)',
   hintDescription: 'Description for the recording (optional)',
 
-  // About screen
-  aboutSubtitle: 'Thanks for trying our demo!',
-  // about_text:'
-  // \nVisit &lt;a href=https://github.com/Ziggeo/Android-Client-SDK>GitHub&lt;/a> if you want to star us or just to see the sources
-  // \n\nYou might also want to take a look at other sections.
-  // \n\nLearn:
-  // \n- &lt;a href=https://ziggeo.com/quickstart>Quick start&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/screencasts>Screencasts&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/webinars>Webinars&lt;/a>
-  // \n\nReference:
-  // \n- &lt;a href=https://ziggeo.com/docs>Documentation&lt;/a>
-  // \n- &lt;a href=https://support.ziggeo.com/>FAQs&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/integrations>Integrations&lt;/a>
-  // \n\nSandboxes
-  // \n- &lt;a href=https://ziggeo.com/sandbox/configuration>Configuration&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/sandbox/serverside>Server-Side&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/sandbox/authorization-tokens>Authorization Tokens&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/sandbox/webhooks>Webhooks Builder&lt;/a>
-  // \n\nCommunity
-  // \n- &lt;a href=https://support.ziggeo.com/hc/en-us/community/posts>Forum&lt;/a>
-  // \n- &lt;a href=https://stackoverflow.com/search?q=ziggeo>StackOverflow&lt;/a>
-  // \n\nSupport
-  // \n- &lt;a href=mailto:support@ziggeo.com>Support&lt;/a>
-  // \n- &lt;a href=https://ziggeo.com/dev-updates>Dev Updates&lt;/a>
-  // ',
+  //TODO refactor to use color from one place
+  aboutSubtitle: '<h2>Thanks for trying our demo!<h2>',
+  aboutText: `
+  Visit <a href=https://github.com/Ziggeo/Android-Client-SDK>GitHub</a> if you want to star us or just to see the sources
+  
+  You might also want to take a look at other sections.
+  
+  Learn:
+  - <a style="color:#DC5163;" href=https://ziggeo.com/quickstart>Quick start</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/screencasts>Screencasts</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/webinars>Webinars</a>
+  
+  Reference:
+  - <a style="color:#DC5163;" href=https://ziggeo.com/docs>Documentation</a>
+  - <a style="color:#DC5163;" href=https://support.ziggeo.com/>FAQs</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/integrations>Integrations</a>
+  
+  Sandboxes
+  - <a style="color:#DC5163;" href=https://ziggeo.com/sandbox/configuration>Configuration</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/sandbox/serverside>Server-Side</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/sandbox/authorization-tokens>Authorization Tokens</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/sandbox/webhooks>Webhooks Builder</a>
+  
+  Community
+  - <a style="color:#DC5163;" href=https://support.ziggeo.com/hc/en-us/community/posts>Forum</a>
+  - <a style="color:#DC5163;" href=https://stackoverflow.com/search?q=ziggeo>StackOverflow</a>
+  
+  Support
+  - <a style="color:#DC5163;" href=mailto:support@ziggeo.com>Support</a>
+  - <a style="color:#DC5163;" href=https://ziggeo.com/dev-updates>Dev Updates</a>`,
 
   // Contact us
   btnStartNowText: 'Click here to \nSTART RIGHT NOW.',
--- src/screens/About.js
+++ src/screens/About.js
@@ -1,7 +1,22 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Strings from '../Strings';
+import HTML from 'react-native-render-html';
 
-export const About = ({navigation}) => <View style={styles.container} />;
+const htmlContent = `
+    <h1>This HTML snippet is now rendered with native components !</h1>
+    <h2>Enjoy a webview-free and blazing fast application</h2>
+    <img src="https://i.imgur.com/dHLmxfO.jpg?2" />
+    <em style="textAlign: center;">Look at how happy this native cat is</em>
+`;
+
+export const About = ({navigation}) => (
+  <View style={styles.container}>
+    <HTML html={Strings.aboutSubtitle} />
+    <HTML style={{fontSize: 16}} html={Strings.aboutText} />
+    {/*<HTML html={htmlContent} />*/}
+  </View>
+);
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
