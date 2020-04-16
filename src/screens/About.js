a2c23e3af4762cad826a15d051a79c255dbe8325
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-04-16 20:09:59 +0300

    About screen converted from function to React.Component

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,20 +3,16 @@ import React from 'react';
 import Strings from '../Strings';
 import HTML from 'react-native-render-html';
 
-const htmlContent = `
-    <h1>This HTML snippet is now rendered with native components !</h1>
-    <h2>Enjoy a webview-free and blazing fast application</h2>
-    <img src="https://i.imgur.com/dHLmxfO.jpg?2" />
-    <em style="textAlign: center;">Look at how happy this native cat is</em>
-`;
-
-export const About = ({navigation}) => (
-  <View style={styles.container}>
-    <HTML html={Strings.aboutSubtitle} />
-    <HTML style={{fontSize: 16}} html={Strings.aboutText} />
-    {/*<HTML html={htmlContent} />*/}
-  </View>
-);
+export class About extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HTML html={Strings.aboutSubtitle} />
+        <HTML style={{fontSize: 16}} html={Strings.aboutText} />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
