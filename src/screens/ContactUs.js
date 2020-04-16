30d99371864f81c0f721fab2201eeecbd8082257
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-04-16 20:25:19 +0300

    ContactUs screen converted from function to React.Component.

--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,12 +1,35 @@
-import {StyleSheet, View} from 'react-native';
 import React from 'react';
+import Strings from '../Strings';
+import {Button} from 'react-native-elements';
+import {StyleSheet, View} from 'react-native';
+import Theme from '../Theme';
 
-export const ContactUs = ({navigation}) => <View style={styles.container} />;
+export class ContactUs extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          buttonStyle={styles.btn}
+          title={Strings.btnStartNowText}
+          onPress={this.onStartBtnPressed}
+        />
+      </View>
+    );
+  }
+
+  onStartBtnPressed = () => {};
+}
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    alignItems: 'center',
     flex: 1,
+    padding: Theme.size.commonMargin,
+    alignItems: 'center',
+  },
+  btn: {
+    marginTop: Theme.size.btnStartNowTopMargin,
+    backgroundColor: Theme.colors.primary,
+    height: Theme.size.btnQrHeight,
+    width: Theme.size.btnQrWidth,
   },
 });
