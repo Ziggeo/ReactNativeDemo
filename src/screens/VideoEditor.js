e5fecc10885442b8e792bec0072faae51a7872ca
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-04-16 20:44:35 +0300

    VideoEditor screen reimplemented as React.Component

--- src/screens/VideoEditor.js
+++ src/screens/VideoEditor.js
@@ -1,7 +1,11 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
 
-export const VideoEditor = ({navigation}) => <View style={styles.container} />;
+export class VideoEditor extends React.Component {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
