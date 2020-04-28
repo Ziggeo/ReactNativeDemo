80756d540bd35910eae2bbcaefdaef01f8b011b7
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-04-28 07:55:36 +0300

    Added UI for video editor

--- src/screens/VideoEditor.js
+++ src/screens/VideoEditor.js
@@ -1,15 +1,43 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
-
+import {Toolbar, Button} from 'react-native-material-ui';
+import Theme from '../Theme';
+import Strings from '../Strings';
+import Ziggeo from 'react-native-ziggeo-library';
 export class VideoEditor extends React.Component {
   render() {
-    return <View style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        {this.renderToolbar()}
+        <Button
+          style={{
+            container: {
+              marginTop: Theme.size.btnSelectFileMarginTop,
+              backgroundColor: Theme.colors.primary,
+            },
+            text: {
+              color: Theme.colors.white,
+            },
+          }}
+          text={Strings.btnSelectFileText}
+        />
+      </View>
+    );
+  }
+  renderToolbar() {
+    return (
+      <Toolbar
+        style={{container: {backgroundColor: Theme.colors.primary}}}
+        onLeftElementPress={() => this.props.navigation.openDrawer()}
+        leftElement="menu"
+        centerElement={Strings.titleVideoEditor}
+      />
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
-    paddingTop: 40,
     alignItems: 'center',
     flex: 1,
   },
