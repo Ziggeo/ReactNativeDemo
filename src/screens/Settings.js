f5a01dd05a0450e0615f70de8d500b2c5db1378a
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-04-28 08:04:18 +0300

    Fixed UI for settings screen

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,22 +1,37 @@
 import {StyleSheet, Text, View} from 'react-native';
 import Strings from '../Strings';
 import React from 'react';
+import {Toolbar} from 'react-native-material-ui';
+import Theme from '../Theme';
 
 export class Settings extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>{Strings.comingSoon}</Text>
+        {this.renderToolbar()}
+        <View style={styles.textContainer}>
+          <Text>{Strings.comingSoon}</Text>
+        </View>
       </View>
+    );
+  }
+  renderToolbar() {
+    return (
+      <Toolbar
+        style={{container: {backgroundColor: Theme.colors.primary}}}
+        onLeftElementPress={() => this.props.navigation.openDrawer()}
+        leftElement="menu"
+        centerElement={Strings.titleSettings}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    paddingTop: 40,
+  container: {height: '100%'},
+  textContainer: {
     alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
   },
 });
