38197ed8db8c3c2935c3493aeb20561b394bba18
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-04-28 08:16:47 +0300

    Fixed UI for ContactUs and About screens

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,13 +2,29 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Strings from '../Strings';
 import HTML from 'react-native-render-html';
+import {Toolbar} from 'react-native-material-ui';
+import Theme from '../Theme';
 
 export class About extends React.Component {
+  renderToolbar() {
+    return (
+      <Toolbar
+        style={{container: {backgroundColor: Theme.colors.primary}}}
+        onLeftElementPress={() => this.props.navigation.openDrawer()}
+        leftElement="menu"
+        centerElement={Strings.titleAbout}
+      />
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <HTML html={Strings.aboutSubtitle} />
-        <HTML style={{fontSize: 16}} html={Strings.aboutText} />
+      <View style={{height: '100%'}}>
+        {this.renderToolbar()}
+        <View style={styles.container}>
+          <HTML html={Strings.aboutSubtitle} />
+          <HTML style={{fontSize: 16}} html={Strings.aboutText} />
+        </View>
       </View>
     );
   }
--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -3,16 +3,31 @@ import Strings from '../Strings';
 import {Button} from 'react-native-elements';
 import {StyleSheet, View} from 'react-native';
 import Theme from '../Theme';
+import {Toolbar} from 'react-native-material-ui';
 
 export class ContactUs extends React.Component {
+  renderToolbar() {
+    return (
+      <Toolbar
+        style={{container: {backgroundColor: Theme.colors.primary}}}
+        onLeftElementPress={() => this.props.navigation.openDrawer()}
+        leftElement="menu"
+        centerElement={Strings.titleContact}
+      />
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          buttonStyle={styles.btn}
-          title={Strings.btnStartNowText}
-          onPress={this.onStartBtnPressed}
-        />
+      <View style={{height: '100%'}}>
+        {this.renderToolbar()}
+        <View style={styles.container}>
+          <Button
+            buttonStyle={styles.btn}
+            title={Strings.btnStartNowText}
+            onPress={this.onStartBtnPressed}
+          />
+        </View>
       </View>
     );
   }
