0c6ea2646697c6ffc4719120352cdb6ed35892db
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-05-13 15:10:09 +0300

    Dependency fixed to prevent warnings.

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,4 @@
-import {AsyncStorage} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const appTokenKey = 'appToken';
 export const getAppToken = () => AsyncStorage.getItem(appTokenKey);
