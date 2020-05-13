636fe7156df105480802cb555563cfdd9d4933dd
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-05-13 15:27:09 +0300

    Added coming soon text to video editor screen

--- src/screens/VideoEditor.js
+++ src/screens/VideoEditor.js
@@ -1,29 +1,21 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Toolbar, Button} from 'react-native-material-ui';
 import Theme from '../Theme';
 import Strings from '../Strings';
-import Ziggeo from 'react-native-ziggeo-library';
+
 export class VideoEditor extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         {this.renderToolbar()}
-        <Button
-          style={{
-            container: {
-              marginTop: Theme.size.btnSelectFileMarginTop,
-              backgroundColor: Theme.colors.primary,
-            },
-            text: {
-              color: Theme.colors.white,
-            },
-          }}
-          text={Strings.btnSelectFileText}
-        />
+        <View style={styles.textContainer}>
+          <Text>{Strings.comingSoon}</Text>
+        </View>
       </View>
     );
   }
+
   renderToolbar() {
     return (
       <Toolbar
@@ -35,10 +27,12 @@ export class VideoEditor extends React.Component {
     );
   }
 }
+
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
+  container: {height: '100%'},
+  textContainer: {
     alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
   },
 });
