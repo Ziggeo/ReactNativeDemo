19101a46d791aa04e53dc46d97569b0a9fb058de
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-06-03 08:57:41 +0300

    Removed unused code.

--- src/screens/main/reducers.js
+++ src/screens/main/reducers.js
@@ -6,14 +6,6 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    //     case Types.VALIDATION_ERR:
-    //       return {...state, tokenValidationError: action.payload};
-    //     case Types.LOGIN_SUCCESS:
-    //       return {...state, user: action.payload};
-    //     case Types.USER_LOADED:
-    //       return {...INITIAL_STATE, user: action.payload};
-    //     case Types.LOG_OUT:
-    //       return {...INITIAL_STATE};
     default:
       return {...state};
   }
