6fa5d7261a3f93f8121d8c3148a603c3f43efb47
Severyn <severyn@ziggoe.com>
2021-04-05 13:45:40 -0400

    Changed name of font from quicksand_regular to Quicksand-Regular, Changed colors of SafeAreaViews

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import React from "react";
 import {AppRegistry, SafeAreaView} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import Theme from "./src/Theme";
 
-const SafeAreaApp = () => <SafeAreaView style={{ flex: 1 }}><App/></SafeAreaView>
+const SafeAreaApp = () => (
+    <>
+        <SafeAreaView style={{flex: 0, backgroundColor: Theme.colors.background }}/>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
+            <App/>
+        </SafeAreaView>
+    </>
+)
 
 AppRegistry.registerComponent(appName, () => SafeAreaApp);
--- src/ui/textFontStyle.js
+++ src/ui/textFontStyle.js
@@ -2,6 +2,6 @@ import {StyleSheet} from 'react-native';
 
 export const textFontStyle = StyleSheet.create({
   textStyle: {
-    fontFamily: 'quicksand_regular',
+    fontFamily: 'Quicksand-Regular',
   },
 });
