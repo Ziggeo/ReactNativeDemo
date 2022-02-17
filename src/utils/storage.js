6089d0aee881754ea163109a7fb21636fc2ce41a
Alex Bedulin <alexander.bedulin@gmail.com>
2022-02-17 16:21:30 +0300

    Added blur mode settings

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage';
+import Ziggeo from 'react-native-ziggeo-library';
+import {error, receiveRecs} from '../screens/recordings/actions';
 
 const appTokenKey = 'appToken';
 export const getAppToken = () => AsyncStorage.getItem(appTokenKey);
@@ -6,25 +8,27 @@ export const saveAppToken = user => AsyncStorage.setItem(appTokenKey, user);
 export const removeAppToken = () => AsyncStorage.removeItem(appTokenKey);
 
 const customVideoMode = 'customVideoMode';
-export const getCustomVideoMode = () => AsyncStorage.getItem(customVideoMode).then(value => {
+export const getCustomVideoMode = () =>
+  AsyncStorage.getItem(customVideoMode).then(value => {
     if (value == null) {
-        value = false
+      value = false;
     }
-    return JSON.parse(value)
-})
+    return JSON.parse(value);
+  });
 export const saveCustomVideoMode = value =>
-    AsyncStorage.setItem(customVideoMode, JSON.stringify(value));
+  AsyncStorage.setItem(customVideoMode, JSON.stringify(value));
 
 const customCameraMode = 'customCameraMode';
-export const getCustomCameraMode = () => AsyncStorage.getItem(customCameraMode).then(value => {
+export const getCustomCameraMode = () =>
+  AsyncStorage.getItem(customCameraMode).then(value => {
     if (value == null) {
-        value = false
+      value = false;
     }
-    return JSON.parse(value)
-})
+    return JSON.parse(value);
+  });
 
 export const saveCustomCameraMode = value =>
-    AsyncStorage.setItem(customCameraMode, JSON.stringify(value));
+  AsyncStorage.setItem(customCameraMode, JSON.stringify(value));
 
 const isBlurMode = 'isBlurMode';
 export const getBlurMode = () =>
@@ -35,5 +39,7 @@ export const getBlurMode = () =>
     return JSON.parse(value);
   });
 
-export const saveBlurMode = value =>
+export const saveBlurMode = value => {
   AsyncStorage.setItem(isBlurMode, JSON.stringify(value));
+  Ziggeo.setBlurMode(value);
+};
