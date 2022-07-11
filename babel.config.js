c62df145c7ee15868730675007d34d7862d8da63
Alex Bedulin <alexander.bedulin@gmail.com>
2022-07-11 10:49:44 +0300

    fixed babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  presets: [['module:metro-react-native-babel-preset', {
-    unstable_disableES6Transforms: true
-  }]],
-  plugins: [
-    "react-native-reanimated/plugin",
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      {
+        unstable_disableES6Transforms: true,
+      },
+    ],
   ],
+  plugins: ['react-native-reanimated/plugin'],
 };
